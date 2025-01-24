448dcd2233da14ef11e18535acb6da39ee15378e
athreyacodes <athreyamr10@gmail.com>
2025-01-24 15:50:21 +0530

    bg update

--- src/app/base/background/background.component.ts
+++ src/app/base/background/background.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BACKFROUND_IMAGE } from '../../constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-background',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './background.component.html',
   styleUrl: './background.component.scss'
 })
 export class BackgroundComponent {
-
+  bgStyle = {'background': 'url(' + BACKFROUND_IMAGE + ') 100px 250px'};
+  bgStyle2 = 'url(' + BACKFROUND_IMAGE + ') 100px 250px';
 }
